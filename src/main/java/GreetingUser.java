7279708bcf5d96ff12d5ed829b6f110d5bcaa76d
Ilya Lyskovets <lyskovetsiv@gmail.com>
2021-01-24 18:22:25 +0300

    add user greeting class

---
+++ src/main/java/GreetingUser.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class GreetingUser {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        System.out.println("Hello " + scanner.nextLine() + "!");
+    }
+}

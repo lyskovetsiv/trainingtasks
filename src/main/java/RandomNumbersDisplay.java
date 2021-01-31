39c99e9778565418b5438fa3573c94ce7d7b7a7d
Ilya Lyskovets <lyskovetsiv@gmail.com>
2021-01-31 19:54:28 +0300

    add RandomNumbersDisplay task

---
+++ src/main/java/RandomNumbersDisplay.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumbersDisplay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numberOfRandomArguments;
+
+        System.out.print("Enter number of random arguments: ");
+        numberOfRandomArguments = scanner.nextInt();
+        System.out.print("\nArguments in line: ");
+
+        for (int i = 0; i < numberOfRandomArguments; i++) {
+            System.out.print(random.nextInt(100) + " ");
+        }
+
+        System.out.println("\nArguments in column: ");
+
+        for (int i = 0; i < numberOfRandomArguments; i++) {
+            System.out.println(random.nextInt(100) + " ");
+        }
+    }
+}

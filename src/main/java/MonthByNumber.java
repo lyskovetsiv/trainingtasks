4007f006a49c4384fa490a31903ce06a3b4d8da6
Ilya Lyskovets <lyskovetsiv@gmail.com>
2021-02-01 20:09:44 +0300

    add MonthByNumber task

---
+++ src/main/java/MonthByNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MonthByNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] months = new String[] {"January", "February", "March", "April", "May",
+                "June", "July", "August", "September", "October", "November", "December"};
+        int monthNumber;
+
+        System.out.print("Enter a month number: ");
+        monthNumber = scanner.nextInt();
+        System.out.println(months[monthNumber - 1]);
+    }
+}

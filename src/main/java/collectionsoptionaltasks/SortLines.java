e10458bc06c3da73f4f825d678a769b48eb1351e
Ilya Lyskovets <lyskovetsiv@gmail.com>
2021-02-09 21:21:01 +0300

    add sorting lines task

---
+++ src/main/java/collectionsoptionaltasks/SortLines.java
@@ -0,0 +1,23 @@
+package collectionsoptionaltasks;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class SortLines {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/resources/poem");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = bufferedReader.readLine();
+        ArrayList<String> contentFromFile = new ArrayList<>();
+
+        while (line != null) {
+            contentFromFile.add(line);
+            line = bufferedReader.readLine();
+        }
+        Collections.sort(contentFromFile);
+        System.out.println(contentFromFile);
+    }
+}

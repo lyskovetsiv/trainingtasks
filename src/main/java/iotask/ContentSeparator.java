e74d5fd5677533b92f2d0e1da61006df27f24831
Ilya Lyskovets <lyskovetsiv@gmail.com>
2021-04-14 16:44:24 +0300

    finish io task

--- src/main/java/iotask/ContentSeparator.java
+++ src/main/java/iotask/ContentSeparator.java
@@ -1,49 +1,66 @@
 package iotask;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 
 public class ContentSeparator {
     File folder;
     File[] listOfContent;
-    int numberOfDirectories = 0;
-    int numberOfFiles = 0;
-    int averageNumberOfFilesInDirectory;
-    int averageLengthOfFileName;
+    BufferedWriter writer;
+    String resultsFilePath = "src/main/resources/contentSeparatorFile.txt";
+    FileReader fileReader = new FileReader();
 
-    public void showFilesInAFolder(String folderPath) {
+
+    {
+        try {
+            writer = new BufferedWriter(new FileWriter(resultsFilePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createDirectoryStructureFile(String folderPath) {
         folder = new File(folderPath);
         listOfContent = folder.listFiles();
         int fileCounter = 1;
-        String indentForDirectories = "---";
+        String indentForDirectories = "------";
 
-        assert listOfContent != null;
+        try {
+            writer.newLine();
 
-        for (File entity : listOfContent) {
-            if (entity.isFile()) {
-                System.out.println("(" + fileCounter + ")" + entity.getName());
-                fileCounter++;
-                numberOfFiles++;
-            }
-        }
-            for (File entity1 : listOfContent) {
-                if (entity1.isDirectory()) {
-                    System.out.println();
-                    System.out.println(indentForDirectories + entity1.getName());
-                    folder = new File(folderPath + "\\" + entity1.getName());
-                    numberOfDirectories++;
-                    showFilesInAFolder(folder.getPath());
+            for (File entity : listOfContent) {
+                if (entity.isFile()) {
+                    writer.write("   (" + fileCounter + ")" + entity.getName() + "\n");
+                    fileCounter++;
                 }
             }
+            for (File entity1 : listOfContent) {
+                if (entity1.isDirectory()) {
+                    writer.newLine();
+                    writer.write(indentForDirectories + entity1.getName());
+                    folder = new File(folderPath + "\\" + entity1.getName());
+                    createDirectoryStructureFile(folder.getPath());
+                }
+            }
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
     public void showTreeOfAFolder(String folderPath) {
-        if (folderPath.equals("D:\\justForPractice\\results.txt")) {
-            System.out.println(numberOfDirectories);
-            System.out.println(numberOfFiles);
-        }
-        else {
-            System.out.println(folderPath);
-            showFilesInAFolder(folderPath);
+        try {
+            if (folderPath.equals("src/main/resources/results.txt")) {
+                fileReader.getContentFromFile();
+            } else {
+                writer.write(folderPath);
+                createDirectoryStructureFile(folderPath);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/iotask/FolderContent.java
+++ src/main/java/iotask/FolderContent.java
@@ -4,6 +4,7 @@ public class FolderContent {
     public static void main(String[] args) {
         ContentSeparator contentSeparator = new ContentSeparator();
 
-        contentSeparator.showTreeOfAFolder("D:\\justForPractice");
+        contentSeparator.showTreeOfAFolder("D:\\JustForPractice");
+        contentSeparator.showTreeOfAFolder("src/main/resources/results.txt");
     }
 }
